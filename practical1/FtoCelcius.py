5a8435fa8dc38984641af8dbd7d9b05182f15d16
MorningCoral <35279431+MorningCoral@users.noreply.github.com>
2018-01-13 22:51:48 +0800

    Update FtoCelcius.py

--- practical1/FtoCelcius.py
+++ practical1/FtoCelcius.py
@@ -1,3 +1,4 @@
+# Convert fahrenheit to Celcius
 fahrenheit = float(input("Temperature in Fahrenheit: "))
 celcius = (5/9)*(fahrenheit - 32)
 print ("Temperature in Celcius: {0:.1f}".format(celcius))
